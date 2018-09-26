f7753d20785441761317c2489f01716edd1a5aaa
Jagan <jmohanr33@gmail.com>
2018-09-26 16:12:14 +0530

    AddedTestingApis

---
+++ ImageEditing/ImageProcessing/TestingApis.swift
@@ -0,0 +1,21 @@
+//
+//  TestingApis.swift
+//  ImageProcessing
+//
+//  Created by Admin on 26/09/2018.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import Foundation
+
+///https://www.googleapis.com/books/v1/volumes?filter=free-ebooks&q=a
+//https://api.coinmarketcap.com/v1/ticker/
+//http://www.kaleidosblog.com/swift-uitableview-load-data-from-json
+//https://simplifiedcoding.net/demos/marvel/
+//https://www.simplifiedios.net/custom-uitableviewcell-tutorial/
+//https://gist.github.com/Starefossen/689428b6c532d7fec0bb
+//https://theappspace.com/google-maps-sdk-ios-using-swift-4/
+//https://medium.com/m/global-identity?redirectUrl=https://engineering.door2door.io/dotted-polylines-with-google-maps-sdk-for-ios-7b21786a0645
+//https://stackoverflow.com/questions/42136203/how-to-draw-routes-between-two-locations-in-google-maps-ios-swift?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+//https://stackoverflow.com/questions/22550849/drawing-route-between-two-places-on-gmsmapview-in-ios/35045733#35045733
+//http://irekasoft.com/blog/google-maps-sdk-ios
